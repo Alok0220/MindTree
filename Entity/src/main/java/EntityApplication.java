460c9fb01551a993ed41a4891651d30b477de138
alok <musykworld@gmail.com>
2020-08-24 21:13:47 +0530

    recent changes

--- Entity/src/main/java/EntityApplication.java
+++ Entity/src/main/java/EntityApplication.java
@@ -3,9 +3,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
+ * Copyright (C) Joffer Systems, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alok Dethe <dethealok@gmail.com>, <det.alex0110@gmail.com>, July 2020
+
  */
 
 
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MindTree
 Educational Application, Demo Application.
+
+## About MindTree
+*This is demo app, This is not a complete app, For complete app <dethealok11@gmail.com>, <det.alex0110@gmail.com>*
