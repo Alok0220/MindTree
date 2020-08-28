badf0009d46ccc74aac82dc086121885fb98495e
alok <musykworld@gmail.com>
2020-08-29 02:18:16 +0530

    recent

--- Entity/src/main/java/com/treemode/school/entity/model/Parents.java
+++ Entity/src/main/java/com/treemode/school/entity/model/Parents.java
@@ -8,6 +8,7 @@
  */
 package com.treemode.school.entity.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -23,13 +24,17 @@ import javax.persistence.Table;
 public class Parents {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column( name = "id" )
     private Long id;
 
+    @Column( name = "first_name" )
     private String firstName;
     
+    @Column( name = "last_name" )
     private String lastName;
     
+    @Column( name = "address" )
     private String address;
 
     public String getFirstName() {
