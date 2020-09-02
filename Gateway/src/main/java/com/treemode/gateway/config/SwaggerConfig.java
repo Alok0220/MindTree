d4a87accd45120da9867eaa25605e014af0735fa
alok <musykworld@gmail.com>
2020-09-03 02:16:26 +0530

    recent

---
+++ Gateway/src/main/java/com/treemode/gateway/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+/*
+
+ * Copyright (C) Joffer Systems, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alok Dethe <dethealok@gmail.com>, <det.alex0110@gmail.com>, July 2020
+
+ */
+package com.treemode.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ *
+ * @author alokdethe
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
+    
+}
---
+++ Gateway/src/main/java/com/treemode/gateway/serviceImpl/SchoolServiceImpl.java
@@ -0,0 +1,25 @@
+/*
+
+ * Copyright (C) Joffer Systems, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alok Dethe <dethealok@gmail.com>, <det.alex0110@gmail.com>, July 2020
+
+ */
+package com.treemode.gateway.serviceImpl;
+
+
+import java.util.Optional;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author alokdethe
+ */
+@Service
+public class SchoolServiceImpl {
+
+  
+    
+}
